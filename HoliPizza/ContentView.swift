90a92e237f5d7325d3e6770ee6cec4b1f4559b9a
urvi-k <urvi151212@gmail.com>
2024-03-03 15:21:41 -0500

    init project

--- HoliPizza/ContentView.swift
+++ HoliPizza/ContentView.swift
@@ -9,13 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack() {
+           Text("Huli Pizza")
+            Image("tiger")
+                .resizable()
+                .scaledToFit()
+            HStack {
+                Text("Order Pizza")
+                    .bold()
+                    .font(.title2)
+                Spacer()
+            }
+            HStack {
+                Text("Order Items")
+                Spacer()
+                Text(12.78, format: .currency(code: "CAD"))
+            }
+            HStack(alignment: .top,spacing: 10) {
+                Image("pz1")
+                    .resizable()
+                    .scaledToFit()
+                VStack(alignment: .leading) {
+                    Text("Margherita")
+                    Text("Pizza with paparoni")
+                }
+            }
+            
         }
         .padding()
+        Spacer()
     }
 }
 
