7e5d6f8b7ffe6e17dba119827847b5a6d93407af
urvi-k <urvi151212@gmail.com>
2024-03-03 16:45:39 -0500

    make list of items using foreach, scrollview

--- HoliPizza/ContentView.swift
+++ HoliPizza/ContentView.swift
@@ -8,32 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    var orders: [Int] = []
     var body: some View {
         VStack() {
            Text("Huli Pizza")
             Image("tiger")
                 .resizable()
                 .scaledToFit()
+            Image(systemName: orders.isEmpty ? "cart" : "cart.fill") // SF Symbols
+            
             HStack {
-                Text("Order Pizza")
-                    .bold()
-                    .font(.title2)
+                    Text("Order Pizza")
+                        .bold()
+                        .font(.title2)
                 Spacer()
             }
-            HStack {
-                Text("Order Items")
-                Spacer()
-                Text(12.78, format: .currency(code: "CAD"))
-            }
-            HStack(alignment: .top,spacing: 10) {
-                Image("pz1")
-                    .resizable()
-                    .scaledToFit()
-                VStack(alignment: .leading) {
-                    Text("Margherita")
-                    Text("Pizza with paparoni")
+            
+            ScrollView {
+                VStack{
+                    ForEach(1...8, id: \.self) { item in
+                        HStack {
+                            Text("Order Items")
+                            Spacer()
+                            Text(12.78, format: .currency(code: "CAD"))
+                            
+                        }
+                    }
                 }
-            }
+                .padding(.bottom, 30)
+                    ForEach(1...5, id: \.self) { item in  // Use of \.self
+                        HStack(alignment: .top,spacing: 10) {
+                            Image(systemName: "\(item).circle").font(.title2)
+                            Image("pz1")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 80)
+                            VStack(alignment: .leading) {
+                                Text("Margherita")
+                                Text("Pizza with paparoni")
+                            }
+                            Spacer()
+                        }
+                    }
+                }
             
         }
         .padding()
